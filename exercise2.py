41945a0ee198e59a81567f077c50d0df9e00c5a1
Kazaxthan <carlo.paule.29@gmail.com>
2024-01-20 23:26:13 +0800

    second commit, this is where i fiished the task where it will add the current number to the previous number.

--- exercise2.py
+++ exercise2.py
@@ -1,9 +1,16 @@
 #exercise2
 #print the sum of the curret number and the previous number
 print (" printing current and previous number sum in a range (10)")
+
+#string for previuos number
 prev_num = 0
 
 for i in range (1,11):
-
-
-    print (i)
+    #adding the previous number the current number
+    sum= prev_num + i
+    
+    
+    print ("Current number is", i ,"Previous number is", prev_num,  "sum is", sum)
+    
+    #string for current number
+    prev_num = i
