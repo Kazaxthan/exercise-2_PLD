f7a707929b35590d0d7c01a56423f3f85e614dc4
Kazaxthan <carlo.paule.29@gmail.com>
2024-01-20 23:18:39 +0800

    i've coded where it will print the numbers from 1-10

---
+++ exercise2.py
@@ -0,0 +1,9 @@
+#exercise2
+#print the sum of the curret number and the previous number
+print (" printing current and previous number sum in a range (10)")
+prev_num = 0
+
+for i in range (1,11):
+
+
+    print (i)
